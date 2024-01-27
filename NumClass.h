ed392cbd1104bfe6e9d90d7cb0ee3706b11b7fa7
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-01-27 19:07:35 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -1,6 +1,3 @@
-#ifndef NUMCLASS_H
-#define NUMCLASS_H
-
 int isStrong(int n);
 int isPalindrome(int n);
 int isArmstrong(int n);
@@ -8,5 +5,3 @@ int isPrime(int n);
 int power(int n,int p);
 int reverse(int n);
 int isArmstrongHelper(int n,int m);
-
-#endif
